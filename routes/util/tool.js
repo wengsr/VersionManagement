6ccf956a1c24a9203cd3daed00a9a0bc45487062
lijuanzhang <1021890251@qq.com>
2015-07-16 10:12:15 +0800

    变更单导出

---
+++ routes/util/tool.js
@@ -0,0 +1,23 @@
+/**
+ * Created by lijuanZhang on 2015/7/10.
+ */
+
+var express = require('express');
+
+/**
+ * 从cookie中获取user给session，如果session中user为空，就返回主页
+ * @param req
+ * @param res
+ * @returns {*}
+ */
+exports.getCookieUser = function(req, res){
+    var cookieUser = req.cookies.user;
+    if(cookieUser){
+        req.session.user = cookieUser;
+    }
+    if(!req.session.user || 'undefined'==req.session.user){
+        return res.redirect("/");
+    }
+}
+
+
