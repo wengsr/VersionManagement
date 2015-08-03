f0838b7840efd33c209e9d3a7f283435efb0f118
lijuanzhang <1021890251@qq.com>
2015-08-03 10:02:08 +0800

    测试不通过，增加开发人员确认环节

---
+++ modular/sqlStatement/processStepReason.js
@@ -0,0 +1,17 @@
+/**
+ * Created by lijuanzhang on 2015/7/30.
+ */
+var processStepReason = function(){
+    //插入每个环节的原因，
+    this.insertReason = "insert into taskprocessreason(taskid,processStepId,reason,turnNum,testNum) values(?,?,?," +
+    "   (SELECT maxNum from (SELECT MAX(turnNum) as maxNum FROM taskprocessstep where taskId=?) as maxNumTable)," +
+    "   (SELECT maxTestNum from (SELECT MAX(testNum) as maxTestNum FROM taskprocessstep where taskId=? ) as maxTestNumTable))";
+   var insertReason_params ="[taskid,processStepID,reason,taskId,taskid]";
+    //回退到前面某个环节
+    this.insertReason_b = "insert into taskprocessreason(taskid,processStepId,reason,turnNum,testNum) values(?,?,?," +
+    "   (SELECT maxNum from (SELECT MAX(turnNum) as maxNum FROM taskprocessstep where taskId=?) as maxNumTable)," +
+    "   (SELECT maxTestNum from (SELECT MAX(testNum) as maxTestNum FROM taskprocessstep where taskId=? ) as maxTestNumTable)+1)";
+    var insertReasonB_params ="[taskid,processStepID,reason,taskId,taskid]";
+
+}
+module.exports = processStepReason;
