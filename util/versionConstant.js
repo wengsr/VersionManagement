1a1ccfca4317938baf2a19d8a825e140afe65e0c
lijuanzhang <1021890251@qq.com>
2015-11-15 18:52:02 +0800

    版本管理新流程（测试库和发布库）

---
+++ util/versionConstant.js
@@ -0,0 +1,48 @@
+/**
+ * Created by lijuanZhang on 2015/10/29.
+ */
+var versionConstant ={
+    svnLocation:{
+        CHANGATTARepository:1,
+        TESTRepository : "http://192.168.1.22:8000/svn/hxbss/testVersion/a/",
+        DevRepository :"http://192.168.1.22:8000/svn/hxbss/testVersion/a-branch/"
+    },
+    paths :{
+        //DevRepositoryPath:"./svn-branch/"
+        DevRepositoryPath:"D:\\testSvn-branch"
+    },
+    states:{
+        APPLYCOMPLETE  :  "申请完成",
+        FILESEXTRACTED:"旧文件已提取",
+        FILESUBMITED:"变更文件已提交",
+        PLANCHECKED:"已安排走查",
+        CHECK:"走查",
+        SUBMITTING:"正在上库",
+        AUTOSUBMITTED:"已自动上库完成",
+        SUBMITTED:"上库完成",
+        TEST:"测试",
+        TESTED:"测试完成",
+        TESTUNPASS:"测试不通过",
+        BUGCOMFIRMED:"BUG确认",
+        SUBMITTODEV:"上发布库",
+        AUTOSUBMITTODEV:"已自动上发布库",
+        SUBMITTODEVFAIL:"上发布库失败",
+        SUBMITTODEVCOMPLETE:"上发布库完成"
+    },
+    processStep:{
+        APPLY:1,
+        EXTRACTFILE:2,
+        SUBMITFILE:3,
+        PLANCHECK:4,
+        CHECK:5,
+        SUBMIT:6,
+        SUBMTICOMPLETE:7,
+        TEST:8,
+        TESTCEOMPLET:9,
+        COMFIRMING:10,
+        CONFIRM:11,
+        SUBMITTODEV:12,
+        SUBMITTODEVCOMPLETE:13
+    }
+}
+module.exports = versionConstant;
